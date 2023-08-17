cbc29047ce7d6edc75546ca6491dfaa31e02c749
abarre18 <114777312+abarre18@users.noreply.github.com>
2023-08-17 19:42:44 +0200

    Add files via upload

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarre <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 16:29:50 by albarre           #+#    #+#             */
+/*   Updated: 2023/08/17 16:38:32 by albarre          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j < argc)
+	{
+		i = 0;
+		while(argv[j][i] != '\0')
+		{
+			ft_putchar(argv[j][i]);
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+	return (0);
+}
